91caa1cd5ab53c8c454a0e77ecfc1b26a80b0abb
joaobatistafrontend <jbbuno007@gmail.com>
2022-09-05 10:34:11 -0300

    add form,add msg do form no html, add msg no back

---
+++ core/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from .models import Servico, Funcionario, Features, Sobre
+from .forms import ContatoForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
-class IndexView(TemplateView):
+
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -10,4 +16,27 @@ class IndexView(TemplateView):
         context ['funcionarios'] = Funcionario.objects.order_by('?').all()
         context ['features'] = Features.objects.order_by('?').all()
         context ['sobre'] = Sobre.objects.all()
-        return context 
+        return context
+
+    def form_valid(self, form, *args,**kwargs):
+
+        form = ContatoForm(self.request.POST or None)
+        if self.request.method == 'POST':
+            if form.is_valid():
+                nome = form.cleaned_data['nome']
+                email = form.cleaned_data['email']
+                assunto = form.cleaned_data['assunto']
+                mensagem = form.cleaned_data['mensagem']
+
+                print(f'Nome: {nome}\nE-mail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}')
+
+                messages.success(self.request,'E-mail enviado com sucesso!')
+            else:
+                messages.error('Falha ao envio do E-mail')
+
+            return super(IndexView, self).form_valid(form,*args, **kwargs)
+
+
+
+
+
